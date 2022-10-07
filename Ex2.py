4f281eb87346e56718090dd430b3db115ec57454
KreoX <soldimksol@gmail.com>
2022-10-07 17:00:38 +0300

    Ex1

---
+++ Ex2.py
@@ -0,0 +1,15 @@
+import random
+num = int(input('Введите количество элементов списка: '))
+array= []
+for i in range(num):
+    array.append(random.randint(1,10))
+print (array)
+mult= []
+length=int(num/2)
+if num%2==0:
+    for j in range(length):
+        mult.append(array[j]*array[-j-1])
+else:
+    for j in range(length+1):
+        mult.append(array[j]*array[-j-1])
+print(mult)
