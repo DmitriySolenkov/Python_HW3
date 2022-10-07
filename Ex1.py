2b628c71792d654d0d242e6fc177638943b457d0
KreoX <soldimksol@gmail.com>
2022-10-07 17:02:14 +0300

    Format

--- Ex1.py
+++ Ex1.py
@@ -1,11 +1,11 @@
 import random
 num = int(input('Введите количество элементов списка: '))
-array= []
+array = []
 for i in range(num):
-    array.append(random.randint(1,10))
-print (array)
-sum=0
+    array.append(random.randint(1, 10))
+print(array)
+sum = 0
 for j in range(num):
-    if j%2!=0:
-        sum+=array[j]
-print (sum)
+    if j % 2 != 0:
+        sum += array[j]
+print(sum)
--- Ex2.py
+++ Ex2.py
@@ -1,12 +1,12 @@
 import random
 num = int(input('Введите количество элементов списка: '))
-array= []
+array = []
 for i in range(num):
-    array.append(random.randint(1,10))
-print (array)
-mult= []
-length=int(num/2)
-if num%2==0:
+    array.append(random.randint(1, 10))
+print(array)
+mult = []
+length = int(num/2)
+if num % 2 == 0:
     for j in range(length):
         mult.append(array[j]*array[-j-1])
 else:
