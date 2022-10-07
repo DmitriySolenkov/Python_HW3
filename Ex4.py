90ee1391ea267c6f2e910452e6a56889a1a90559
KreoX <soldimksol@gmail.com>
2022-10-07 17:34:40 +0300

    Format

--- Ex4.py
+++ Ex4.py
@@ -1,10 +1,10 @@
 num = int(input('Введите число: '))
-array=[]
-while num!=1:
-    array.append(int(num%2))
-    num=int(num/2)
+array = []
+while num != 1:
+    array.append(int(num % 2))
+    num = int(num/2)
 array.append(1)
-string= ""
+string = ""
 for j in range(len(array)):
-    string+=str(array[-j-1])
-print (string)
+    string += str(array[-j-1])
+print(string)
